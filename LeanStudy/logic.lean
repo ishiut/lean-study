d39e8ab8b65c37cdbfe378c3146495b4d6109de3
Tetsuya Ishiu <ishiut@miamioh.edu>
2026-06-13 03:36:13 +0000

    Jun 12, 2026, 11:36 PM [skip ci]

--- LeanStudy/logic.lean
+++ LeanStudy/logic.lean
@@ -5,3 +5,13 @@ theorem double_negation (P : Prop) (h : ¬¬P) : P := by
   contradiction
 
 #print axioms double_negation
+
+open Classical
+noncomputable section
+-- variable {α : Type*} [DecidableEq α] (a : α) (s t : Finset α)
+variable {α : Type*} (a : α) (s t : Finset α)
+
+#check a ∈ s
+#check s ∩ t
+
+end
